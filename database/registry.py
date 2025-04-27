96daa8f79dd81799fa0f82d53ddf5ceebfd47c82
valentin-qrw <EVELINA-VALENTYNA.KOZAK@lnu.edu.ua>
2025-04-27 13:08:21 +0300

    ghuegherfie

--- database/registry.py
+++ database/registry.py
@@ -1,13 +1,18 @@
-from database.database import Database
-
 class DatabaseRegistry:
-    """Реєстр баз даних."""
+    """Глобальний реєстр баз даних."""
     _databases = {}
 
     @classmethod
     def register(cls, name, db_instance):
+        """Реєструє базу даних під заданим ім'ям."""
         cls._databases[name] = db_instance
 
     @classmethod
     def get(cls, name):
+        """Повертає базу даних за ім'ям."""
         return cls._databases.get(name)
+
+    @classmethod
+    def list(cls):
+        """Повертає список усіх зареєстрованих баз даних."""
+        return list(cls._databases.keys())
